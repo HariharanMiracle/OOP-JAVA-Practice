5354efe2369fff004841e006a0291f34b718e367
HariharanMiracle <hariharankim@gmail.com>
2021-02-23 11:30:15 +0530

    thread cont

---
+++ src/thread/ThreadPriority2.java
@@ -0,0 +1,20 @@
+package thread;
+
+public class ThreadPriority2 {
+    public static void main(String args[]){
+        ThreadA tA = new ThreadA();
+        ThreadA tB = new ThreadA();
+        tA.setPriority(Thread.MAX_PRIORITY);
+        tB.setPriority(Thread.MIN_PRIORITY);
+        tA.setName("Thread A");
+        tB.setName("Thread B");
+        tA.start();
+        tB.start();
+    }
+}
+
+class ThreadA extends Thread{
+    public void run(){
+        System.out.println("Thread: " + Thread.currentThread().getName() + " - " + Thread.currentThread().getPriority());
+    }
+}
